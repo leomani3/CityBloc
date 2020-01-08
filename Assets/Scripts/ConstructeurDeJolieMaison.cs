a14bea83939779730791f6b3cab90c28b63cad40
Leomani <leoveyssiere.pro@hotmail.fr>
2020-01-08 15:18:02 +0100

    Classe building

--- Assets/Scripts/ConstructeurDeJolieMaison.cs
+++ Assets/Scripts/ConstructeurDeJolieMaison.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ConstructeurDeJolieMaison : MonoBehaviour
 {
+    public List<Vector3> coordinates;
+
+    private MeshRenderer meshRenderer;
+    private MeshFilter meshFilter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshFilter = GetComponent<MeshFilter>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Construct()
+    {
+
     }
 }
