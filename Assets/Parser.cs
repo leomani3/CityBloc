7f33abce1c7d57433ff327ba1dd7c9cda40881ea
TheWincher <louchez26@hotmail.fr>
2020-01-09 16:05:09 +0100

    petite amélioration du parser

--- Assets/Parser.cs
+++ Assets/Parser.cs
@@ -8,20 +8,22 @@ public class Parser
 {
     public static List<Building> GetBuildingsFromJSON(string path)
     {
+        JObject encodedBuilding = JObject.Parse(File.ReadAllText(Application.dataPath + "/Bati_JSON_RGF93_LITE.geojson"));
+        JArray features = (JArray) encodedBuilding["features"];
+
         List<Building> buildings = new List<Building>();
-        JObject jObject = JObject.Parse(File.ReadAllText(Application.dataPath + "/Bati_JSON_RGF93_LITE.geojson"));
-        JArray features = (JArray) jObject["features"];
-        foreach(JObject j in features)
+        foreach(JObject feature in features)
         {
-            Debug.Log("features : " + features.Count);
-            float val = (float) j["properties"]["HAUTEUR"];
-            JArray coordJSON = (JArray) j["geometry"]["coordinates"][0];
+            float val = (float) feature["properties"]["HAUTEUR"];
+            JArray coordJSON = (JArray) feature["geometry"]["coordinates"][0];
+
             List<Vector2> coords = new List<Vector2>();
             foreach(JArray j1 in coordJSON)
             {
                 coords.Add(new Vector2((float) j1[0], (float) j1[1]));
             }
-            Building b = new Building((float) j["properties"]["HAUTEUR"], coords);
+            
+            Building b = new Building((float) feature["properties"]["HAUTEUR"], coords);
             buildings.Add(b);
         }
         return buildings;
