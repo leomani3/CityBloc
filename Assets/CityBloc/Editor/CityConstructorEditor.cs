b70977eed662b79f8743e2f16a9f7334d5480424
Corentin <corentin.chignin@hotmail.fr>
2020-05-12 10:51:32 +0200

    Add editor script
    Can build city without play in editor

---
+++ Assets/CityBloc/Editor/CityConstructorEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ConstructCity))]
+public class CityConstructorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Build city"))
+        {
+            ((ConstructCity)target).BuildCity();
+        }
+    }
+}
--- Assets/CityBloc/Scripts/ConstructCity.cs
+++ Assets/CityBloc/Scripts/ConstructCity.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class ConstructCity : MonoBehaviour
 {
+    //Building list of your city
     List<Building> city;
+
+    //Prefab of houses 
     public HouseConstructor houseConstructorPrefab;
-    public string path;
-    void Start()
+
+    //Geojson file of your city
+    public TextAsset geojsonFile;
+
+    public void BuildCity()
     {
-        city = Parser.GetBuildingsFromJSON(Application.dataPath + path);
+        city = Parser.GetBuildingsFromJSON(Application.dataPath + "/CityBloc/Scripts/" + geojsonFile.name + ".json");
 
         Vector2 cityCenter = new Vector2();
         int nb = 0;
@@ -26,12 +32,11 @@ public class ConstructCity : MonoBehaviour
 
         for (int i = 0; i < city.Count; i++)
         {
-            //Debug.Log(city[i].points[0].x);
             HouseConstructor house = Instantiate(houseConstructorPrefab, new Vector3((city[i].points[0].x - cityCenter.x) / 10000, 0, (city[i].points[0].y - cityCenter.y) / 10000), Quaternion.identity);
             house.building = city[i];
             house.cityCenter = cityCenter;
-            //Debug.Log(gerardInstancier.building.points[0].x);
             house.Construct();
+
         }
     }
 }
